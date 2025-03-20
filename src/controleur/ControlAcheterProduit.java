7db55b772b8708eeadeb48fdeaa8e58acd5eb9a1
Pierre-Marie COMBALBERT <pierremarie.combalbert@gmail.com>
2025-03-20 18:39:13 +0100

    End tp3

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -2,6 +2,7 @@ package controleur;
 
 import frontiere.Clavier;
 import personnages.Gaulois;
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -21,16 +22,31 @@ public class ControlAcheterProduit {
 		if(controlVerifierIdentite.verifierIdentite(nomAcheteur)) {
 			String produit = Clavier.entrerChaine("Quel produit voulez-vous acheter ?");
 			Gaulois[] gaulois = village.rechercherVendeursProduit(produit);
-			if(gaulois.length > 0) {
+			if(gaulois != null && gaulois.length > 0) {
 				StringBuilder bld = new StringBuilder();
-				bld.append("Chez quel commerçant vouslez-+vous acheter des fleurs ?\n");
+				bld.append("Chez quel commerçant voulez-vous acheter des fleurs ?\n");
 				for(int i = 0; i < gaulois.length;i++) {
-					bld.append(" - " + gaulois[i].getNom()+"\n");
+					bld.append((i+1) + " - " + gaulois[i].getNom()+"\n");
 				}
-				int etal = Clavier.entrerEntier(bld.toString());
-				//todo ajouter test étal
-				System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " + gaulois[etal].getNom());
-				int nbProduit = Clavier.entrerEntier("Combien de " + produit + " voulez-vous acheter ?");
+				int numGaulois;
+				do {
+					numGaulois = Clavier.entrerEntier(bld.toString());
+				} while(numGaulois < 0 || numGaulois > gaulois.length);
+				String nomVendeur = gaulois[numGaulois-1].getNom();
+				System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " +nomVendeur);
+				int nbProduit = Clavier.entrerEntier("Bonjour "+ nomAcheteur + "\nCombien de " + produit + " voulez-vous acheter ?");
+				Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+				if(etal.getQuantite() == 0) {
+					System.out.println(nomAcheteur + " veut acheter " + nbProduit + " " + produit + ", malheuresement il n'y en a plus !");
+				}else if(etal.getQuantite() < nbProduit) {
+					System.out.println(nomAcheteur + " veut acheter " + nbProduit + " " + produit + ", malheuresement " + nomVendeur + " n'en a plus que " + etal.getQuantite() + " " + nomAcheteur + " achète tout le stock de " + nomVendeur);
+					etal.acheterProduit(etal.getQuantite());
+				} else if (etal.getQuantite() >= nbProduit) {
+					etal.acheterProduit(nbProduit);
+					System.out.println(nomAcheteur + " achète 5 fleurs à " + nomVendeur);
+				}
+				
+				
 			} else {
 				System.out.println("Désolé personne ne vend ce produit au marché");
 			}
